744f3f7c4eeda55a4644278a6d3d97da3b398a23
tocxjo <tocjo@riseup.net>
2024-11-28 03:14:33 -0300

    initial commit

---
+++ init.el
@@ -0,0 +1,20 @@
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("296dcaeb2582e7f759e813407ff1facfd979faa071cf27ef54100202c45ae7d4" "ffa78fc746f85d1c88a2d1691b1e37d21832e9a44a0eeee114a00816eabcdaf9" "a3a71b922fb6cbf9283884ac8a9109935e04550bcc5d2a05414a58c52a8ffc47" "b3ba955a30f22fe444831d7bc89f6466b23db8ce87530076d1f1c30505a4c23b" "b1791a921c4f38cb966c6f78633364ad880ad9cf36eef01c60982c54ec9dd088" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "ac893acecb0f1cf2b6ccea5c70ea97516c13c2b80c07f3292c21d6eb0cb45239" "ea4dd126d72d30805c083421a50544e235176d9698c8c541b824b60912275ba1" "cd5f8f91cc2560c017cc9ec24a9ab637451e36afd22e00a03e08d7b1b87c29ca" "b41d0a9413fb0034cea34eb8c9f89f6e243bdd76bccecf8292eb1fefa42eaf0a" "d6b369a3f09f34cdbaed93eeefcc6a0e05e135d187252e01b0031559b1671e97" "1ad12cda71588cc82e74f1cabeed99705c6a60d23ee1bb355c293ba9c000d4ac" "e85a354f77ae6c2e47667370a8beddf02e8772a02e1f7edb7089e793f4762a45" "691d671429fa6c6d73098fc6ff05d4a14a323ea0a18787daeb93fde0e48ab18b" "00d7122017db83578ef6fba39c131efdcb59910f0fac0defbe726da8072a0729" "ae20535e46a88faea5d65775ca5510c7385cbf334dfa7dde93c0cd22ed663ba0" "cee5c56dc8b95b345bfe1c88d82d48f89e0f23008b0c2154ef452b2ce348da37" "b33955472cb61a721c59c705afda11fea906756d38b3c2eba61698f4e3f82897" "9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "e8bd9bbf6506afca133125b0be48b1f033b1c8647c628652ab7a2fe065c10ef0" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641" default))
+ '(jabber-avatar-max-height 5)
+ '(jabber-avatar-max-width 5)
+ '(package-selected-packages
+   '(doom-modeline pomodoro circe keepass jabber flycheck vterm elcord ivy-youtube ytdl elfeed holy-books twittering-mode evil-org evil-collection keepass-mode org-ai org-roam-bibtex org-bullets all-the-icons gptel ssh-deploy gcmh transmission enlight magit yasnippet magit-p4 magithub ef-themes org-fragtog tron-legacy-theme lsp-mode aggressive-indent marginalia vertico which-key doom-themes org-roam-ui org-roam auto-complete org-modern general evil-leader))
+ '(pomodoro-desktop-notification t)
+ '(pomodoro-show-number t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
